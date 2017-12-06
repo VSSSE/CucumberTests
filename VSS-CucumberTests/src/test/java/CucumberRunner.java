661cfca23fb37281a29a66228bc3c8f0dbf51fa7
c0d3d3v <live.for.program@gmail.com>
2017-12-06 17:39:39 +0100

    Adding nice Log Messages

--- VSS-CucumberTests/src/test/java/CucumberRunner.java
+++ VSS-CucumberTests/src/test/java/CucumberRunner.java
@@ -5,7 +5,13 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/Resources", strict = true)
+@CucumberOptions(
+        features="src/test/Resources/FirstTwo/",//path to the features
+        format = {"json:target/integration_cucumber.json"},//what formatters to use
+        tags = {"@run"})
 
 public class CucumberRunner {
+
+
+
 }
--- VSS-CucumberTests/src/test/java/StepDefs.java
+++ VSS-CucumberTests/src/test/java/StepDefs.java
@@ -2,7 +2,9 @@ package test.java;
 
 
 import static org.junit.Assert.fail;
- 
+
+import java.util.logging.Logger;
+
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
@@ -12,6 +14,8 @@ import cucumber.api.java.en.When;
 
 public class StepDefs {
    // public static WebDriver driver = null;
+   private static final Logger LOGGER = Logger.getLogger(StepDefs.class.getName());
+
 
     @Before
     public void setup() {
@@ -28,50 +32,56 @@ public class StepDefs {
     @Given("^I'm at the start page$")
     public void user_is_on_start_page() {
         // TODO
-        fail("Not implemented");
+    	LOGGER.info("Entering: I'm at the start page");
     }
 
 
     @When("^I enter a movie title \"([^\"]*)\"$")
     public void user_enters_a_movie_title(String arg1) throws Throwable {
         // TODO
-        fail("Not implemented");
+    	LOGGER.info("Entering: I enter a movie title '"
+    			+ arg1 + "'");
     }
 
     @And("^I press the search soundtracks button$")
     public void user_presses_the_soundtracks_search_button() throws Throwable {
-        fail("Not implemented");
+
+    	LOGGER.info("Entering: I press the search soundtracks button");
 
     }
 
     @Then("^I get a list of soundtracks$")
     public void user_gets_the_list_of_songs() throws Throwable {
         // TODO
-        fail("Not implemented");
+    	LOGGER.info("Entering: I get a list of soundtracks");
     }
 
     @Then("^I see the message \"([^\"]*)\"$")
     public void user_sees_a_message(String arg1) throws Throwable {
         // TODO
-        fail("Not implemented");
+    	LOGGER.info("Entering: I see the message '" 
+    			+ arg1 + "'");
     }
 
     @Given("^I see the soundtracks list for the movie \"([^\"]*)\"$")
     public void movie_soundtrack_was_found(String arg1){
 
-        fail("Not implemented");
+    	LOGGER.info("Entering: I see the soundtracks list for the movie '" 
+    			+ arg1 + "'");
 
     }
 
     @When("^I click on the first soundtrack$")
     public void user_presses_the_show_similar_songs_button() throws Throwable {
-        fail("Not implemented");
+
+    	LOGGER.info("Entering: I click on the first soundtrack");
 
     }
 
     @Then("^I see a list of similar songs$")
     public void user_gets_the_list_of_similar_songs() throws Throwable {
-        fail("Not implemented");
+
+    	LOGGER.info("Entering: I see a list of similar songs");
 
     }
 
